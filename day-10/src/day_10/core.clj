b99425d5ce88b97af36945f6fdf911e9f1252656
Peter Lindvall <peter.lindvall@gmail.com>
2023-02-02 21:48:03 +0100

    minor fixes

--- day-10/src/day_10/core.clj
+++ day-10/src/day_10/core.clj
@@ -10,15 +10,13 @@
 
 (def input (slurp "resources/test.txt"))
 (def str-v (string/split-lines input))
-(count str-v)
 (def input-map-list (map (fn [r]
                            (let [[cmd value] (string/split r #" ")]
                              (if value
                                {:cmd cmd, :value (read-string value)}
                                {:cmd cmd}))) str-v))
 
-;Add to input map how many cycles each command will use before the result of it is available
-(def complete-data-structure (loop [input input-map-list
+(def add-cycle-cycles-and-x-reg (loop [input input-map-list
                                     output-map-list ()
                                     cycles 0
                                     x-reg 1]
@@ -36,14 +34,15 @@
                                           new-cycles
                                           (map-x-reg :x-reg))))))
 
-(def poi-seq (take 100 (filter #(or (= 20 %) (= 0 (mod (+ 20 %) 40))) (range))))
+(def poi-seq (take 100 (filter #(or (= 20 %) (= 0 (mod (- % 20) 40))) (range))))
 
 (def with-signal-strengths (loop [poi poi-seq
-       maps complete-data-structure
+       maps add-cycle-cycles-and-x-reg
        new-map-list ()]
-  (let [at-poi? (<= (first poi) ((first maps) :last-cycle-used))
-        current-map (first maps)
-        signal-strength {:signal-strength (* (first poi) (current-map :x-reg))}
+  (let [current-map (first maps)
+        current-poi (first poi)
+        at-poi? (<= current-poi (current-map :last-cycle-used))
+        signal-strength {:signal-strength (* current-poi (current-map :x-reg))}
         next-poi (if at-poi? (rest poi) poi)]
     (if (= 1 (count maps))
       new-map-list
@@ -52,10 +51,9 @@
              (if at-poi? (conj new-map-list (conj current-map signal-strength))
                          (conj new-map-list current-map)))))))
 
-(reduce (fn [sum el]
-          (+ sum (el :signal-strength))) 0 (filter #(contains? % :signal-strength) with-signal-strengths))
 
 (defn -main
   "Solution for day 10"
   [& args]
-  (println "Hello, World!"))
+  (println (reduce (fn [sum el]
+            (+ sum (el :signal-strength))) 0 (filter #(contains? % :signal-strength) with-signal-strengths))))
